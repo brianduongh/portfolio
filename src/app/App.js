28bce06fa63d5e33d0f1d8c911fb93d6c2cf7b3f
Brian Duong <brianduongh@gmail.com>
2018-09-14 18:07:26 -0500

    contact

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import NavigationBar from '../components/navigation';
@@ -8,19 +8,24 @@ import Skills from '../components/skills';
 import Projects from '../components/projects';
 import Contact from '../components/contact';
 
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <NavigationBar />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/about" component={About} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+class App extends Component {
+  render() {
+    const { env } = this.props;
+    return (
+      <BrowserRouter>
+        <div className="app">
+          <NavigationBar />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/skills" component={Skills} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} env={env} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormGroup, FormControl } from 'react-bootstrap';
+import { FormGroup, FormControl, Button } from 'react-bootstrap';
 import './contact.css';
 
 class Contact extends Component {
@@ -9,9 +9,25 @@ class Contact extends Component {
     message: '',
   }
 
+  handleCancel = this.handleCancel.bind(this);
+
+  handleSubmit = this.handleSubmit.bind(this);
+
   handleChange = key => (event) => {
     const { target: { value } } = event;
     this.setState({ [key]: value });
+  }
+
+  handleCancel() {
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+    });
+  }
+
+  handleSubmit() {
+    console.log('submmited');
   }
 
   render() {
@@ -20,39 +36,41 @@ class Contact extends Component {
       <section id="contact">
         <div className="contact">
           <div className="contact-form">
-          <h1>
+            <h1>
             Would you like to
-            <span className="highlight"> contact </span>
+              <span className="highlight"> contact </span>
             me?
-          </h1>
-          <hr />
-          <form>
-            <FormGroup>
-              <FormControl
-                type="text"
-                placeholder="name"
-                value={name}
-                onChange={this.handleChange('name')}
-              />
-              <FormControl
-                type="email"
-                placeholder="email"
-                value={email}
-                onChange={this.handleChange('email')}
-              />
-              <FormControl
-                componentClass="textarea"
-                style={{ height: 200 }}
-                placeholder="message"
-                value={message}
-                onChange={this.handleChange('message')}
-              />
-            </FormGroup>
-          </form>
+            </h1>
+            <hr />
+            <form>
+              <FormGroup>
+                <FormControl
+                  type="text"
+                  placeholder="name"
+                  value={name}
+                  onChange={this.handleChange('name')}
+                />
+                <FormControl
+                  type="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={this.handleChange('email')}
+                />
+                <FormControl
+                  componentClass="textarea"
+                  style={{ height: 200 }}
+                  placeholder="message"
+                  value={message}
+                  onChange={this.handleChange('message')}
+                />
+              </FormGroup>
+              <Button onClick={this.handleSubmit}>Submit</Button>
+              <Button onClick={this.handleCancel}>Cancel</Button>
+            </form>
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
 
